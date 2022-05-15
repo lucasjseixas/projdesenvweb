95e6f0a0e75151df373751d5849fffe027c9205b
Lucas José de Alencar Seixas <87376922+xyxwab@users.noreply.github.com>
2022-05-15 00:05:25 -0300

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -27,12 +27,19 @@ let containerprocura = document.getElementById("containerprocura");
 
 let fechar = document.getElementById("fechar");
 
+let alerta = document.getElementById("msgalerta");
+
+let teste = document.getElementById("testediv");
+
+let resultadobusca = document.getElementById("resultadobusca");
+
 // Declaração de eventos --
 // Validação de input de usuário
+
 user.addEventListener("keyup", () => {
   if (user.value.length <= 2) {
     labeluser.setAttribute("style", "color: red");
-    labeluser.innerHTML = "No mínimo 3 caracteres para usuário";
+    labeluser.innerHTML = "<strong>No mínimo 3 caracteres para usuário</strong>";
     user.setAttribute = ("style", "border-color: red");
     validcampuser = false;
   } else {
@@ -48,7 +55,7 @@ user.addEventListener("keyup", () => {
 password.addEventListener("keyup", () => {
   if (password.value.length <= 2) {
     labelpassword.setAttribute("style", "color: red");
-    labelpassword.innerHTML = "Senha de pelo menos 3 caracteres";
+    labelpassword.innerHTML = "<strong>Senha de pelo menos 3 caracteres</strong>";
     password.setAttribute = ("style", "border-color: red");
     validcamppassword = false;
   } else {
@@ -59,71 +66,117 @@ password.addEventListener("keyup", () => {
   }
 });
 
-mostrarlogin2.addEventListener("click", ()=>{
-  if(containerlogin.style.display === "" || containerlogin.style.display === "none"){
-    containerlogin.style.display = "block";
-  } else {
-    containerlogin.style.display = "none";
-  }
-});
+// Mostrar Tela de Login -- JavaScript puro
 
-mostrarlogin.addEventListener("click", ()=>{
-  if(containerlogin.style.display === "" || containerlogin.style.display === "none"){
-    containerlogin.style.display = "block";
-  } else {
-    containerlogin.style.display = "none";
-  }
-});
+// mostrarlogin2.addEventListener("click", ()=>{
+//   if(containerlogin.style.visibility === "" || containerlogin.style.visibility === "hidden"){
+//     containerlogin.style.visibility = "visible";
+//   } else {
+//     containerlogin.style.visibility = "hidden";
+//   }
+// });
 
-fechar.addEventListener("click", ()=>{
-  containerlogin.style.display = "none";
-});
+// mostrarlogin.addEventListener("click", ()=>{
+//   if(containerlogin.style.visibility === "" || containerlogin.style.visibility === "hidden"){
+//     containerlogin.style.visibility = "visible";
+//   } else {
+//     containerlogin.style.visibility = "hidden";
+//   }
+// });
+
+// fechar.addEventListener("click", ()=>{
+//   if(containerlogin.style.visibility === "" || containerlogin.style.visibility === "visible")
+//   listofusers.innerHTML = "";
+//   containerlogin.style.visibility = "hidden";
+// });
 
 // Declaração de funções --
 // Função de checagem de LocalStorage --
 
-function checaruser() {
-  if (!localStorage.getItem("token")) {
-    window.alert("Erro: Usuário nao logado");
-  } else {
-    window.alert("Bem vindo(a) de volta");
+// function checaruser() {
+//   if (!localStorage.getItem("token")) {
+//     console.log("Usuário nao logado");
+//   } else {
+//     console.log("Bem vindo(a) de volta");
+//     teste.innerHTML = "Bem vindo(a) de volta";
+//   }
+// };
+
+$(document).ready(function(){
+  if(!localStorage.getItem("token")){
+    console.log("Erro: Usuário não logado");
+  }else{
+    console.log("Bem vindo(a) de volta");
+    $("#containerprocura").show();
+    $("#testediv").hide();
   }
-};
+});
 
 function sair() {
   localStorage.removeItem("token");
   if(!localStorage.removeItem("token")){
-  window.alert("Usuário deslogado com sucesso");
   labeluser.style.display = "";
+  labeluser.innerHTML = "";
   user.style.display = "";
   labelpassword.style.display = "";
+  labelpassword.innerHTML = "";
   password.style.display = "";
   userlogado = false;
   containerprocura.style.display = "none"
   listofactivities.innerHTML = "";
+  listofusers.innerHTML = "";
+  teste.style.display = "block";
+  resultadobusca.innerHTML = "";
+  resultadobusca.style.visibility = "hidden";
+  resultadobusca.style.fontSize = "0px";
   }
+  if(userlogado === false){
+    alerta.style.visibility = "visible";
+    alerta.innerHTML = "<strong>Usuário deslogado!</strong>";
+  };
 };
 
 // Request API --
 // Request da lista de usuários da API
 
-userlist.addEventListener("click", () => {
-  fetch("https://reqres.in/api/users?page=1")
-    .then((resp) => resp.json())
-    .then((resp) => {
-      console.log(resp.data);
-      for (var i = 0; i < resp.data.length; i++) {
-        let li = document.createElement("li"),
-          emailf = resp.data[i].email;
-        li.innerHTML = emailf;
-        listofusers.appendChild(li);
+
+// JavaScript puro
+
+// userlist.addEventListener("click", () => {
+//   fetch("https://reqres.in/api/users?page=1")
+//     .then((resp) => resp.json())
+//     .then((resp) => {
+//       console.log(resp.data);
+//       for (var i = 0; i < resp.data.length; i++) {
+//         let li = document.createElement("li"),
+//           emailf = resp.data[i].email;
+//         li.innerHTML = emailf;
+//         listofusers.appendChild(li);
+//       }
+//     });
+// });
+
+// Jquery Lista de Usuarios --
+
+$(document).ready(function(){
+  $("#userlist").click(function(){
+  $.ajax({
+  url: "https://reqres.in/api/users?page=1",
+  type: "GET",
+  success: function(response){
+    for(var i = 0; i<response.data.length; i++){
+      let emailf = response.data[i].email;
+      $("<li id='myli'>" + emailf + "</li>").appendTo("#listofusers").hide().slideDown(600);
+        } 
       }
-    });
+    })
+  })
 });
 
 // Request Login na API
 
 entrar.addEventListener("click", () => {
+  if(validcamppassword === true && validcampuser === true){
   fetch("https://reqres.in/api/login", {
     method: "POST",
     headers: {
@@ -134,37 +187,35 @@ entrar.addEventListener("click", () => {
       password: password.value,
     }),
   }).then((resp) => {
-    let respstatus = resp.status;
     if (resp.status === 200) {
       resp.json().then((resp) => {
-        localStorage.setItem("token", resp.token),
-          window.alert(
-            "Usuário " + user.value + " logado com sucesso!" + " " + respstatus
-          )
-          msgsuccesso.setAttribute('style', 'display: block')
-          msgsuccesso.innerHTML = '<strong>Usuário logado</strong>'
-          msgerro.setAttribute('style', 'display: none')
-          msgerro.innerHTML = ''
+          localStorage.setItem("token", resp.token);
+          // msgsuccesso.setAttribute('style', 'display: block');
+          // msgsuccesso.innerHTML = '<strong>Usuário logado</strong>';
+          // msgerro.setAttribute('style', 'display: none');
+          // msgerro.innerHTML = '';
+          alerta.setAttribute('style', 'color: green');
+          alerta.style.display = "inline-block";
+          alerta.innerHTML = '<strong>Login bem sucedido</strong>';
           containerprocura.style.display = "block";
           userlogado = true;
+          teste.style.display = "none";
       });
     } else {
-      window.alert("Erro: Usuário ou senha incorretos" + " " + respstatus);
-      msgerro.setAttribute('style', 'display: block')
-      msgerro.innerHTML = '<strong>Erro ao logar...</strong>'
-      msgsuccesso.innerHTML = ''
-      msgsuccesso.setAttribute('style', 'display: none')
+          // msgerro.setAttribute('style', 'display: block');
+          // msgerro.innerHTML = 'Erro 400: Usuário ou senha incorretos';
+          // msgsuccesso.innerHTML = '';
+          // msgsuccesso.setAttribute('style', 'display: none');
+          alerta.setAttribute('style', 'color: red');
+          alerta.style.display = 'inline-block';
+          alerta.innerHTML = '<strong>Erro ao logar ou usuário já logado</strong>';
+          teste.style.display = "block";
     }
-    
-  labeluser.style.display = "none";
-  user.style.display = "none";
-  labelpassword.style.display = "none";
-  password.style.display = "none";
-
-  });
+  })
   password.value = '';
   user.value = '';
-});
+}});
+
 
 // Get de API pública
 
@@ -172,10 +223,11 @@ document.querySelector("#listadeatividades").addEventListener("click", ()=>{
   fetch("https://www.boredapi.com/api/activity")
     .then((resposta) => resposta.json())
     .then((resposta) =>{
-        let li = document.createElement("li");
         d = resposta.activity;
-        li.innerHTML = 'Atividade aleatória: ' + d;
-        listofactivities.appendChild(li);
+        resultadobusca.style.fontSize = "17px";
+        resultadobusca.innerHTML = "";
+        resultadobusca.style.visibility = "visible";
+        resultadobusca.innerHTML = 'Atividade aleatória: ' + d;
     })
 });
 
@@ -184,10 +236,11 @@ document.getElementById("procurarporchave").addEventListener("click", ()=>{
   fetch("https://www.boredapi.com/api/activity?key=" + atividadeporchaveid.value)
   .then((resp) => resp.json())
   .then((resp)=>{
-    let li = document.createElement("li");
     d = resp.activity;
-    li.innerHTML = 'Atividade por chave: ' + d;
-    listofactivities.appendChild(li);
+    resultadobusca.innerHTML = "";
+    resultadobusca.style.fontSize = "17px";
+    resultadobusca.style.visibility = "visible";
+    resultadobusca.innerHTML = 'Atividade por chave: ' + d;
   })
   atividadeporchaveid.value = "";
 });
@@ -198,10 +251,11 @@ document.getElementById("procurarportipo").addEventListener("click", ()=>{
   fetch("https://www.boredapi.com/api/activity?type=" + atividadesmenuid.value)
   .then((resp) => resp.json())
   .then((resp)=>{
-    let li = document.createElement("li");
     d = resp.activity;
-    li.innerHTML = 'Atividade por tipo: ' + d;
-    listofactivities.appendChild(li);
+    resultadobusca.innerHTML = "";
+    resultadobusca.style.fontSize = "17px";
+    resultadobusca.style.visibility = "visible";
+    resultadobusca.innerHTML = 'Atividade por tipo: ' + d;
     })
 });
 
@@ -210,11 +264,11 @@ document.getElementById("procurarporparticipantes").addEventListener("click", ()
   fetch("https://www.boredapi.com/api/activity?participants=" + atividadeporparticipantesid.value)
   .then((resp) => resp.json())
   .then((resp)=>{
-    console.log(resp);
-    let li = document.createElement("li");
     d = resp.activity;
-    li.innerHTML = 'Atividade por participantes: ' + d;
-    listofactivities.appendChild(li);
+    resultadobusca.innerHTML = "";
+    resultadobusca.style.fontSize = "17px";
+    resultadobusca.style.visibility = "visible";
+    resultadobusca.innerHTML = 'Atividade por pessoas: ' + d;
     })
     atividadeporparticipantesid.value = "";
 });
@@ -224,11 +278,11 @@ document.getElementById("procurarporpreco").addEventListener("click", ()=>{
   fetch("https://www.boredapi.com/api/activity?price=" + atividadepeloprecoid.value)
   .then((resp) => resp.json())
   .then((resp)=>{
-    console.log(resp);
-    let li = document.createElement("li");
     d = resp.activity;
-    li.innerHTML = 'Atividade por preço: ' + d;
-    listofactivities.appendChild(li);
+    resultadobusca.innerHTML = "";
+    resultadobusca.style.fontSize = "17px";
+    resultadobusca.style.visibility = "visible";
+    resultadobusca.innerHTML = 'Atividade por preço: ' + d;
     })
     atividadepeloprecoid.value = "";
 });
@@ -239,4 +293,50 @@ document.querySelector("#resetarpesquisa").addEventListener("click", ()=>{
   atividadepeloprecoid.value = "";
   atividadeporparticipantesid.value = "";
   atividadeporchaveid.value = "";
+  resultadobusca.innerHTML = "";
+  resultadobusca.style = "";
+});
+
+// Login Botao Desktop
+
+$(document).ready(function(){
+  $("#btn-box1-2-hidden").click(function(){
+    if(userlogado === true){
+    $(".containerprocura").show();
+    $(".containerlogin").show().animate({height: "100%"}, 600);
+    }else{
+      $(".containerlogin").show().animate({height: "100%"}, 600);
+    }
+  })
+});
+
+  $("#fechar").click(function(){
+    if(userlogado === false){
+    $(".containerlogin").hide("slide", {direction: "right"}, 600);
+    $("#listofusers").empty().append("");
+    } else {
+    $(".containerlogin").hide("slide", {direction: "right"}, 600);
+    }
+});
+
+// Login Botao Mobile
+
+$(document).ready(function(){
+  $("#btn-box1-2").click(function(){
+    if(userlogado === true){
+      $(".containerprocura").show();
+      $(".containerlogin").show().animate({height: "100%"}, 600);
+      }else{
+      $(".containerlogin").show().animate({height: "100%"}, 600);
+      }
+  });
+
+  $("#fechar").click(function(){
+    if(userlogado === false){
+      $(".containerlogin").hide("slide", {direction: "right"}, 600);
+      $("#listofusers").empty().append("");
+      } else {
+      $(".containerlogin").hide("slide", {direction: "right"}, 600);
+      }
+  })
 });
